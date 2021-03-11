8c168a6c3b5f8067a60d4452583b5d7caf0ced48
s-ws-go <75929950+s-ws-go@users.noreply.github.com>
2021-03-11 22:22:20 +0900

    #1 cacheimages

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Image, Text } from "react-native";
+import AppLoading from "expo-app-loading";
+import { Asset } from "expo-asset";
+
+//cacheImages 는 image들의 array로 구성 예정
+//else문의 asset은 expo의 내장 라이브러리로, 외부 asset으로의 접근을 허용해 줌
+const cacheImages = (images) => {
+  images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  const [isReady, setIsReady] = useState(false);
+  const loadAssets = async () => {
+    const images = cacheImages([
+      "https://images.unsplash.com/photo-1615450112423-f724345c306f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+      require("./assets/splash.png"),
+    ]);
+    console.log(images);
+  };
+
+  const onFinish = () => {
+    setIsReady(true);
+  };
+  return isReady ? (
+    <Text>Ready!</Text>
+  ) : (
+    <AppLoading
+      startAsync={loadAssets}
+      onFinish={onFinish}
+      onError={console.error}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
