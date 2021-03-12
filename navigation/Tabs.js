4ea721a7dea5319e22f68ec6433f7c6ec1a2da54
s-ws-go <75929950+s-ws-go@users.noreply.github.com>
2021-03-12 16:55:48 +0900

    #5 Updating Header

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
 import Search from "../screens/Search";
 import Fav from "../screens/Fav";
+import { getFocusedRouteNameFromRoute } from "@react-navigation/native";
 
 const Tab = createBottomTabNavigator();
 
-export default () => (
-  <Tab.Navigator>
-    <Tab.Screen name="Movies" component={Movies} />
-    <Tab.Screen name="Tv" component={Tv} />
-    <Tab.Screen name="Search" component={Search} />
-    <Tab.Screen name="Fav" component={Fav} />
-  </Tab.Navigator>
-);
+export default ({ navigation, route }) => {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: getFocusedRouteNameFromRoute(route) || "Movies",
+    });
+  }, [navigation, route]);
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Movies" component={Movies} />
+      <Tab.Screen name="Tv" component={Tv} />
+      <Tab.Screen name="Search" component={Search} />
+      <Tab.Screen name="Fav" component={Fav} />
+    </Tab.Navigator>
+  );
+};
