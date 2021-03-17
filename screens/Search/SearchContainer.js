dbced359b583eb40202d993aa8721f92e75e5a03
s-ws-go <75929950+s-ws-go@users.noreply.github.com>
2021-03-17 16:22:20 +0900

    #19 ★ searching Logic

--- screens/Search/SearchContainer.js
+++ screens/Search/SearchContainer.js
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components/native";
 import propTypes from "prop-types";
 import SearchPresenter from "./SearchPresenter";
+import { movieApi, tvApi } from "../../Api";
 
-const SearchContainer = () => (
-  <>
-    <SearchPresenter />
-  </>
-);
+const SearchContainer = () => {
+  const [keyword, setKeyword] = useState("");
+  const [result, setResult] = useState({
+    movies: [],
+    movieError: null,
+    shows: [],
+    showsError: null,
+  });
+  const onChange = (text) => setKeyword(text);
+  const search = async () => {
+    const [movies, movieError] = await movieApi.search(keyword);
+    const [shows, showsError] = await tvApi.search(keyword);
+    setResult({
+      movies,
+      movieError,
+      shows,
+      showsError,
+    });
+  };
+  console.log(result);
+
+  return (
+    <>
+      <SearchPresenter
+        {...result}
+        onChange={onChange}
+        onSubmit={search}
+        keyword={keyword}
+      />
+    </>
+  );
+};
 
 export default SearchContainer;
--- screens/Search/SearchPresenter.js
+++ screens/Search/SearchPresenter.js
@@ -1,19 +1,34 @@
+//Presenter 는 UI를 위한 거니까 Search Logic 은 Container component에 적용되어야 함
+
 import React from "react";
 import styled from "styled-components/native";
 import propTypes from "prop-types";
 import Input from "../../component/search/Input";
+import HorizontalSliders from "../../component/HorizontalSliders";
+import Horizontal from "../../component/Horizontal";
 
 const Container = styled.ScrollView`
   background-color: black;
 `;
 
-const Text = styled.Text`
-  color: white;
-`;
-
-const SearchPresenter = () => (
+const SearchPresenter = ({ movies, shows, onChange, onSubmit, keyword }) => (
   <Container>
-    <Input placeholder="Search For Video.." />
+    <Input
+      placeholder={"Search For Video.."}
+      value={keyword}
+      onChange={onChange}
+      onSubmit={onSubmit}
+    />
+    <HorizontalSliders title="영화 검색결과">
+      {movies.map((movie) => (
+        <Horizontal></Horizontal>
+      ))}
+    </HorizontalSliders>
+    <HorizontalSliders title="TV 검색결과">
+      {shows.map((show) => (
+        <Horizontal></Horizontal>
+      ))}
+    </HorizontalSliders>
   </Container>
 );
 
