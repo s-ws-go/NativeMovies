1cdb7c83190bb3ab8c3d1a3b57aa0cd20fd0b0b5
s-ws-go <75929950+s-ws-go@users.noreply.github.com>
2021-03-18 21:57:02 +0900

    #26 Moving the Cards(stop, re-place)

--- screens/Fav/FavPresenter.js
+++ screens/Fav/FavPresenter.js
@@ -1,7 +1,9 @@
+// PanResponder --- React Native의 내장함수. 제스처를 인식하게 해 줌
+
 import React from "react";
 import styled from "styled-components/native";
 import propTypes from "prop-types";
-import { Dimensions } from "react-native";
+import { Dimensions, PanResponder, Animated } from "react-native";
 import { apiImage } from "../../Api";
 
 const { width: WIDTH, height: HEIGHT } = Dimensions.get("window");
@@ -12,25 +14,41 @@ const Container = styled.View`
   align-items: center;
 `;
 
-const Card = styled.View`
-  top: 40px;
-  width: 90%;
-  height: ${HEIGHT / 1.5}px;
-  position: absolute;
-`;
 const Poster = styled.Image`
   border-radius: 30px;
   width: 100%;
   height: 100%;
 `;
 
+const styles = {
+  top: 40,
+  height: HEIGHT / 1.5,
+  width: "90%",
+  position: "absolute",
+};
+
 const FavPresenter = ({ results }) => {
+  const position = new Animated.ValueXY();
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: (eve, { dx, dy }) =>
+      position.setValue({ x: dx, y: dy }),
+  });
   return (
     <Container>
       {results.reverse().map((result) => (
-        <Card key={result.id}>
+        //일반 View에는 Animation을 넣을 수가 없어서 Animated.View로 컴포넌트를 수정 해 줘야 함.
+        <Animated.View
+          style={{
+            ...styles,
+            //transform 넣을 때 배열형태로 넣어줘야 해.(이거는 API 홈페이지에 안 나오는데...)
+            transform: [...position.getTranslateTransform()],
+          }}
+          key={result.id}
+          {...panResponder.panHandlers}
+        >
           <Poster source={{ uri: apiImage(result.poster_path) }} />
-        </Card>
+        </Animated.View>
       ))}
     </Container>
   );
